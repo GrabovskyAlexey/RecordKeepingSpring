e63fcfd2c3e4b13cc2842e49707c014ae9b84b04
GrabovskyAlexey <grabovsky.alexey@gmail.com>
2022-07-12 16:23:59 +0300

    Initial commit

---
+++ src/main/java/ru/grabovsky/recordkeeping/RecordKeepingSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.grabovsky.recordkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordKeepingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordKeepingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/grabovsky/recordkeeping/RecordKeepingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.grabovsky.recordkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordKeepingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
