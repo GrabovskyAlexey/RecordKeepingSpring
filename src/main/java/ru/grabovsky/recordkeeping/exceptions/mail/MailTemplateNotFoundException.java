44df1c76424faf402a5f0ac965c225e43644d69c
GrabovskyAlexey <grabovsky.alexey@gmail.com>
2024-01-05 21:33:00 +0300

    clear

--- src/main/java/ru/grabovsky/recordkeeping/exceptions/mail/MailTemplateNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.grabovsky.recordkeeping.exceptions.mail;
-
-public class MailTemplateNotFoundException extends RuntimeException{
-    public MailTemplateNotFoundException(String message) {
-        super(message);
-    }
-}
